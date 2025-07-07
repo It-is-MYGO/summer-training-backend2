be3a64e65b5e7130b002f0c76cc80bbe99a562dc
clytie-lee <2093064084@qq.com>
2025-07-07 11:16:52 +0800

    urls.py添加

--- pachong/app01/api/urls.py
+++ pachong/app01/api/urls.py
@@ -1,8 +1,11 @@
 # app01/api/urls.py
 
 from django.urls import path
-from .views import crawl_view
+from .views import crawl_view, crawl_and_mix_view, get_database_products, clear_cache_view
 
 urlpatterns = [
     path('crawl/', crawl_view, name='crawl'),
+    path('crawl-mix/', crawl_and_mix_view, name='crawl_and_mix'),
+    path('database/', get_database_products, name='get_database_products'),
+    path('clear-cache/', clear_cache_view, name='clear_cache'),
 ]
